9b5527c3593949eb9ceda459e87595111b9c67b1
wangke <49186456@qq.com>
2020-01-06 22:49:40 +0800

    修复缺少日志包问题

--- demo-springboot-2-shiro/src/main/java/com/alisls/demo/springboot/shiro/realms/MyRealm.java
+++ demo-springboot-2-shiro/src/main/java/com/alisls/demo/springboot/shiro/realms/MyRealm.java
@@ -1,7 +1,10 @@
 package com.alisls.demo.springboot.shiro.realms;
 
-import com.alisls.demo.springboot.shiro.service.UserService;
-import com.alisls.demo.springboot.shiro.utils.EncryUtils;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authc.AuthenticationInfo;
 import org.apache.shiro.authc.AuthenticationToken;
@@ -12,7 +15,8 @@ import org.apache.shiro.realm.AuthorizingRealm;
 import org.apache.shiro.subject.PrincipalCollection;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import java.util.List;
+import com.alisls.demo.springboot.shiro.service.UserService;
+import com.alisls.demo.springboot.shiro.utils.EncryUtils;
 
 /**
  * 自定义 Realm
@@ -22,26 +26,33 @@ public class MyRealm extends AuthorizingRealm {
     @Autowired
     private UserService userService;
 
+    /**
+     * 进行权限校验的时候会调用
+     */
     @Override
     protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principals) {
         // 主体名
         String username = (String) principals.getPrimaryPrincipal();
         // 根据用户名查询角色
         List<String> roles = userService.getRolesByUsername(username);
-
+        
         // 授权信息
         SimpleAuthorizationInfo simpleAuthorizationInfo = new SimpleAuthorizationInfo();
+        
         // 添加用户角色
         simpleAuthorizationInfo.addRoles(roles);
+        //simpleAuthorizationInfo.setRoles(roles);
+        
+        // 添加权限信息 
+        Collection<String> permissions = getPermissionsByRoles();
+        simpleAuthorizationInfo.addStringPermissions(permissions);
+        //simpleAuthorizationInfo.setStringPermissions(stringPermissions);
 
         return simpleAuthorizationInfo;
     }
 
     /**
-     * 用户认证
-     * @param token
-     * @return
-     * @throws AuthenticationException
+     * 用户认证，用户登录的时候会调用
      */
     @Override
     protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken token)
@@ -53,6 +64,14 @@ public class MyRealm extends AuthorizingRealm {
         String encryptedPassword = EncryUtils.getMD5(password);
         SimpleAuthenticationInfo simpleAuthenticationInfo = new SimpleAuthenticationInfo(username, encryptedPassword, null, getName());
         return simpleAuthenticationInfo;
+    }
+    
+    /**
+     * 根据角色查询权限信息
+     * @return
+     */
+    private Set<String> getPermissionsByRoles() {
+    	return new HashSet<String>();
     }
 
 }
